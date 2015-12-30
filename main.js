c02910d8a8f4e67c89c2bdb417720bdb84219f81
Tim Scanlin <tscanlin@gmail.com>
2015-12-29 21:21:52 -0800

    move files and get things polished up a bit more

--- main.js
+++
@@ -1,7 +0,0 @@
-const spawn = require('npm-execspawn');
-const url = process.argv[1];
-
-var app = spawn('electron' + ' ./app.js');
-
-app.stderr.pipe(process.stderr);
-app.stdout.pipe(process.stdout);
